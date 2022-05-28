7ec837a59c51f7dcd538ddde2f5449baa0f1572a
NotCookey <kanao.nishimiya@gmail.com>
2022-05-29 01:34:47 +0530

    Update imGen.py

--- utils/imGen.py
+++ utils/imGen.py
@@ -7,10 +7,11 @@ def getShiba():
         if urls:
             return urls
 
-def getMeows():
-    urls=json.loads(requests.get("https://cataas.com/cat?json=true").content)["url"]
-    if urls:
-        return "https://cataas.com"+urls
+# PATCH API-FUNCTIONCALL-TIMEOUT
+# def getMeows():
+#     urls=json.loads(requests.get("https://cataas.com/cat?json=true").content)["url"]
+#     if urls:
+#         return "https://cataas.com"+urls
 
 def getDog():
     urls=json.loads(requests.get("https://dog.ceo/api/breeds/image/random").content)["message"]
